c9bfe3dc798a8feb1dcbc9603cede2b06ecb08a3
Jacob-Andes <jacob.andes@gmail.com>
2020-12-24 11:35:25 -0800

    finished servers.test.js

--- jasmine-testing-exercises/jasmine-testing-exercises/tip-pool/servers.test.js
+++ jasmine-testing-exercises/jasmine-testing-exercises/tip-pool/servers.test.js
@@ -1,17 +1,36 @@
-describe("Servers test (with setup and tear-down)", function() {
+describe("Servers test (with setup and tear-down)", function () {
   beforeEach(function () {
     // initialization logic
-    serverNameInput.value = 'Alice';
+    serverNameInput.value = "Alice";
   });
 
-  it('should add a new server to allServers on submitServerInfo()', function () {
+  it("should add a new server to allServers on submitServerInfo()", function () {
     submitServerInfo();
 
     expect(Object.keys(allServers).length).toEqual(1);
-    expect(allServers['server' + serverId].serverName).toEqual('Alice');
+    expect(allServers["server" + serverId].serverName).toEqual("Alice");
   });
 
-  afterEach(function() {
-    // teardown logic
+  it("shouldn't add a new server with no input", function () {
+    serverNameInput.value = "";
+    submitServerInfo();
+    expect(Object.keys(allServers).length).toEqual(0);
+  });
+
+  it("should update server table on updateServerTable()", function () {
+    submitServerInfo();
+    let tdList = document.serverTbody.querySelectorAll(
+      "#serverTable tbody tr td"
+    );
+
+    expect(tdList.length).toEqual(3);
+    expect(tdList[0].innerText).toEqual("Alice");
+    expect(tdList[1].innerText).toEqual("$0.00");
+  });
+
+  afterEach(function () {
+    allServers = {};
+    serverId = 0;
+    serverTbody.innerHTML = "";
   });
 });
