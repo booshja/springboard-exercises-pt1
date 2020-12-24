e6b8ef32441a225f18eaa8b20324dc5dbd86f85f
Jacob-Andes <jacob.andes@gmail.com>
2020-12-24 14:12:47 -0800

    added delete button feature for both servers and payments

--- jasmine-testing-exercises/jasmine-testing-exercises/tip-pool/helpers.js
+++ jasmine-testing-exercises/jasmine-testing-exercises/tip-pool/helpers.js
@@ -22,3 +22,24 @@ function appendTd(tr, value) {
 
   tr.append(newTd);
 }
+
+// expects a table row element, appends a newly created td element from the value
+function appendDeleteBtn(tr) {
+  let newDelBtn = document.createElement("td");
+  newDelBtn.className = "deleteBtn";
+  newDelBtn.innerText = "X";
+  newDelBtn.addEventListener("click", deleteLine);
+
+  tr.append(newDelBtn);
+}
+
+function deleteLine(e) {
+  let ele = e.target.closest("tr");
+
+  delete allServers[ele.id];
+  delete allPayments[ele.id];
+
+  ele.parentNode.removeChild(ele);
+  updateServerTable();
+  updateSummary();
+}
