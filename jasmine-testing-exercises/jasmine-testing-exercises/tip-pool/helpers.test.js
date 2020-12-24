9f82f559a21f83e821ad8618b22dfd001e65536a
Jacob-Andes <jacob.andes@gmail.com>
2020-12-24 13:29:42 -0800

    added payments.test.js, updated index to include test

--- jasmine-testing-exercises/jasmine-testing-exercises/tip-pool/helpers.test.js
+++ jasmine-testing-exercises/jasmine-testing-exercises/tip-pool/helpers.test.js
@@ -3,58 +3,58 @@ describe("helpers tests", function () {
     billAmtInput.value = 100;
     tipAmtInput.value = 30;
     submitPaymentInfo();
-  });
 
-  it("should sum the total of tipAmt", function () {
-    expect(sumPaymentTotal("tipAmt")).toEqual(30);
+    it("should sum the total of tipAmt", function () {
+      expect(sumPaymentTotal("tipAmt")).toEqual(30);
 
-    billAmtInput.value = 183.75;
-    tipAmtInput.value = 40.43;
-    submitPaymentInfo();
+      billAmtInput.value = 183.75;
+      tipAmtInput.value = 40.43;
+      submitPaymentInfo();
 
-    expect(sumPaymentTotal("tipAmt")).toEqual(70.43);
-  });
+      expect(sumPaymentTotal("tipAmt")).toEqual(70.43);
+    });
 
-  it("should sum the total of billAmt", function () {
-    expect(sumPaymentTotal("billAmt")).toEqual(100);
+    it("should sum the total of billAmt", function () {
+      expect(sumPaymentTotal("billAmt")).toEqual(100);
 
-    billAmtInput.value = 183.75;
-    tipAmtInput.value = 40.43;
-    submitPaymentInfo();
+      billAmtInput.value = 183.75;
+      tipAmtInput.value = 40.43;
+      submitPaymentInfo();
 
-    expect(sumPaymentTotal("billAmt")).toEqual(283.75);
-  });
+      expect(sumPaymentTotal("billAmt")).toEqual(283.75);
+    });
 
-  it("should sum the total of tipPercent", function () {
-    expect(sumPaymentTotal("tipPercent")).toEqual(30);
+    it("should sum the total of tipPercent", function () {
+      expect(sumPaymentTotal("tipPercent")).toEqual(30);
 
-    billAmtInput.value = 183.75;
-    tipAmtInput.value = 40.43;
-    submitPaymentInfo();
+      billAmtInput.value = 183.75;
+      tipAmtInput.value = 40.43;
+      submitPaymentInfo();
 
-    expect(sumPaymentTotal("tipPercent")).toEqual(52);
-  });
+      expect(sumPaymentTotal("tipPercent")).toEqual(52);
+    });
 
-  it("should calculate the tip percent", function () {
-    expect(calculateTipPercent(100, 30)).toEqual(30);
-    expect(calculateTipPercent(183.75, 40.43)).toEqual(22);
-  });
+    it("should calculate the tip percent", function () {
+      expect(calculateTipPercent(100, 30)).toEqual(30);
+      expect(calculateTipPercent(183.75, 40.43)).toEqual(22);
+    });
 
-  it("should append a new td", function () {
-    let newTr = document.createElement("tr");
-    appendTd(newTr, "check-check");
-    expect(newTr.children.length).toEqual(1);
-    expect(newTr.firstChild.innerHTML).toEqual("check-check");
-  });
+    it("should append a new td", function () {
+      let newTr = document.createElement("tr");
+      appendTd(newTr, "check-check");
+      expect(newTr.children.length).toEqual(1);
+      expect(newTr.firstChild.innerHTML).toEqual("check-check");
+    });
 
-  afterEach(function () {
-    billAmtInput.value = "";
-    tipAmtInput.value = "";
-    paymentTbody.innerHTML = "";
-    summaryTds[0].innerHTML = "";
-    summaryTds[1].innerHTML = "";
-    serverTbody.innerHTML = "";
-    allPayments = {};
-    paymentId = 0;
+    afterEach(function () {
+      billAmtInput.value = "";
+      tipAmtInput.value = "";
+      paymentTbody.innerHTML = "";
+      summaryTds[0].innerHTML = "";
+      summaryTds[1].innerHTML = "";
+      serverTbody.innerHTML = "";
+      allPayments = {};
+      paymentId = 0;
+    });
   });
 });
---
+++ jasmine-testing-exercises/jasmine-testing-exercises/tip-pool/payments.test.js
@@ -0,0 +1,63 @@
+describe("payments tests", function () {
+  beforeEach(function () {
+    billAmtInput.value = 100;
+    tipAmtInput.value = 30;
+  });
+
+  it("should add a new payment on submit", function () {
+    submitPaymentInfo();
+    expect(Object.keys(allPayments).length).toEqual(1);
+    expect(allPayments["payment1"].billAmt).toEqual("100");
+    expect(allPayments["payment1"].tipAmt).toEqual("30");
+    expect(allPayments["payment1"].tipPercent).toEqual(30);
+  });
+
+  it("shouldn't add a new payment on blank submit", function () {
+    billAmtInput.value = "";
+    tipAmtInput.value = "";
+    submitPaymentInfo();
+    expect(Object.keys(allPayments).length).toEqual(0);
+  });
+
+  it("createCurPayment() shouldn't return a value when billAmt or tipAmt is empty", function () {
+    billAmtInput.value = "";
+    submitPaymentInfo();
+    expect(createCurPayment()).not.toBeDefined();
+
+    billAmtInput.value = "100";
+    tipAmtInput.value = "";
+    submitPaymentInfo();
+    expect(createCurPayment()).not.toBeDefined();
+  });
+
+  it("createCurPayment() should create a new payment", function () {
+    let expected = { billAmt: "100", tipAmt: "30", tipPercent: 30 };
+    expect(createCurPayment()).toEqual(expected);
+  });
+
+  it("appendPaymentTable() should add another tr & td's", function () {
+    let payment = createCurPayment();
+    allPayments["payment1"] = payment;
+
+    appendPaymentTable(payment);
+
+    let tdList = document.querySelectorAll("#paymentTable tbody tr td");
+
+    expect(tdList.length).toEqual(3);
+    expect(tdList[0].innerText).toEqual("$100");
+    expect(tdList[1].innerText).toEqual("$30");
+    expect(tdList[2].innerText).toEqual("30%");
+  });
+
+  afterEach(function () {
+    billAmtInput.value = "";
+    tipAmtInput.value = "";
+    paymentTbody.innerHTML = "";
+    summaryTds[0].innerHTML = "";
+    summaryTds[1].innerHTML = "";
+    summaryTds[2].innerHTML = "";
+    serverTbody.innerHTML = "";
+    paymentId = 0;
+    allPayments = {};
+  });
+});
