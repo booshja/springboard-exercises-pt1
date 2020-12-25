d39df8383c9fee2466dedf95d033505d62dd97b2
Jacob-Andes <jacob.andes@gmail.com>
2020-12-24 19:06:26 -0800

    finished filter functions

--- forEach_Map_Filter/app.js
+++ forEach_Map_Filter/app.js
@@ -1,4 +1,4 @@
-const numbers = [1, 2, 3, 5, 10, 22];
+const numbers = [1, 2, 3, 5, 10, 22, 1, 1];
 const words = ["colt", "matt", "tim", "test", "hi", "goodbye", "smile"];
 const keyValArray = [
   { name: "Elie" },
@@ -8,9 +8,9 @@ const keyValArray = [
 ];
 const fullName = [
   { first: "Elie", last: "Schoppik" },
-  { first: "Tim", last: "Garcia" },
+  { first: "Tim", last: "Garcia", isDogOwner: true },
   { first: "Matt", last: "Lane" },
-  { first: "Colt", last: "Steele" },
+  { first: "Colt", last: "Steele", isDogOwner: true },
 ];
 
 function doubleValues(numbers) {
@@ -88,32 +88,56 @@ function valTimesIndex(numbers) {
   return valXIndex;
 }
 
-// Write a function called extractKey which accepts an array of objects and some key and returns a new array with the value of that key in each object.
 function extractKey(keyValArray, key) {
   return keyValArray.map(function (value) {
     return value[key];
   });
 }
 
-// Write a function called extractFullName which accepts an array of objects and returns a new array with the value of the key with a name of “first” and the value of a key with the name of “last” in each object, concatenated together with a space.
 function extractFullName(fullName) {
   return fullName.map(function (person) {
     return person["first"] + " " + person["last"];
   });
 }
 
-//START OF FILTER
-// Write a function called filterByValue which accepts an array of objects and a key and returns a new array with all the objects that contain that key.
-function filterByValue() {}
+function filterByValue(fullName, key) {
+  return fullName.filter(function (first) {
+    return first[key] !== undefined;
+  });
+}
 
-// Write a function called find which accepts an array and a value and returns the first element in the array that has the same value as the second parameter or undefined if the value is not found in the array.
-function find() {}
+function find(numbers, value) {
+  let found = numbers.filter(function (num) {
+    return num === value;
+  });
+  if (found.length > 0) {
+    return found[0];
+  }
+}
 
-// Write a function called findInObj which accepts an array of objects, a key, and some value to search for and returns the first found value in the array.
-function findInObj() {}
+function findInObj(array, key, value) {
+  return (found = array.filter(function (obj) {
+    return obj[key] === value;
+  })[0]);
+}
 
-//Write a function called removeVowels which accepts a string and returns a new string with all of the vowels (both uppercased and lowercased) removed. Every character in the new string should be lowercased.
-function removeVowels() {}
+function removeVowels(string) {
+  const vowels = "aeiou";
+  return string
+    .toLowerCase()
+    .split("")
+    .filter(function (letter) {
+      return vowels.indexOf(letter) === -1;
+    })
+    .join("");
+}
 
-// Write a function called doubleOddNumbers which accepts an array and returns a new array with all of the odd numbers doubled (HINT - you can use map and fitler to double and then filter the odd numbers).
-function doubleOddNumbers() {}
+function doubleOddNumbers(arr) {
+  return arr
+    .filter(function (val) {
+      return val % 2 !== 0;
+    })
+    .map(function (val) {
+      return val * 2;
+    });
+}
