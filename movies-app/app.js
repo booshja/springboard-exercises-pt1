9d7414946b26dc82d3912c7a8e678049a1d5131d
Jacob-Andes <jacob.andes@gmail.com>
2021-01-02 16:42:55 -0800

    created movies-app project. built ui, finished event listener for submit button, todo-event listener for removal buttons, optional challenges

---
+++ movies-app/app.js
@@ -0,0 +1,12 @@
+$("#submit").click(function (event) {
+  event.preventDefault();
+  const title = $("#movie-title").val();
+  const rating = $("#movie-rating").val();
+  const movieItem = $("<li>")
+    .text("Movie: " + title + " Rating: " + rating + "/10  ")
+    .append("<button class='removeBtn'>Remove</button>");
+  $("#ratings-list").append(movieItem);
+  $("input").val("");
+});
+
+//TODO: ADD EVENT LISTENER FOR REMOVING LI'S
