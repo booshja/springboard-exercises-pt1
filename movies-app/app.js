3579048fd27d536e38437b72fd0539f5058a44fa
Jacob-Andes <jacob.andes@gmail.com>
2021-01-03 18:42:56 -0800

    finished movies-app project, including two optional steps

--- movies-app/app.js
+++ movies-app/app.js
@@ -1,15 +1,29 @@
+// Checks the data, and then creates a new LI with a remove button with the input
 $("#submit").click(function (event) {
   event.preventDefault();
-  const title = $("#movie-title").val();
-  const rating = $("#movie-rating").val();
-  const movieItem = $("<li>")
-    .text("Movie: " + title + " Rating: " + rating + "/10  ")
-    .append("<button class='removeBtn'>Remove</button>");
-  $("#ratings-list").append(movieItem);
-  $("input").val("");
+  // Checking the data: at least 2 chars in title, rating is a number and between 0-10,
+  if ($("#movie-title").val().length() > 1) {
+    if (typeof +$("#movie-rating").val() === "number") {
+      if ($("#movie-rating").val() >= 0 && $("#movie-rating").val() <= 10) {
+        const title = $("#movie-title").val();
+        const rating = $("#movie-rating").val();
+        const movieItem = $("<li>")
+          .text("Movie: " + title + " Rating: " + rating + "/10  ")
+          .append("<button class='removeBtn'>Remove</button>");
+        $("#ratings-list").append(movieItem);
+        $("input").val("");
+      } else {
+        alert("Error: Your rating must be a number between 0 and 10!");
+      }
+    } else {
+      alert("Error: Your rating must be a number!");
+    }
+  } else {
+    alert("Error: Your movie title must be at least 2 characters long.");
+  }
 });
 
-//TODO: ADD EVENT LISTENER FOR REMOVING LI'S
-$("#ratings-list").on("click", "li button", function (e) {
+// Listener for the remove buttons in the list
+$("#ratings-list").on("click", "li button", function () {
   $(this).parent().remove();
 });
