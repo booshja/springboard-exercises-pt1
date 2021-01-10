aa570fdbb7eeb25e507181bb2db9a3a1142ac8c1
Jacob-Andes <jacob.andes@gmail.com>
2021-01-09 17:56:25 -0800

    finished AJAX request and intial design, need to append gifs next

--- ajax-giphy-party/app.js
+++ ajax-giphy-party/app.js
@@ -1,1 +1,26 @@
+const api_key = "BUwYvWo6SBuu7pifo3d7PWWm4ubFENYq";
+const limit = 1;
+
 console.log("Let's get this party started!");
+
+$("#submit").on("click", function (e) {
+  e.preventDefault();
+  const term = $("#gifInput").val();
+  getGiphy(term);
+});
+
+async function getGiphy(term) {
+  const q = term;
+  try {
+    const res = await axios.get("https://api.giphy.com/v1/gifs/search", {
+      params: {
+        q,
+        api_key,
+        limit,
+      },
+    });
+    console.log(res);
+  } catch {
+    console.log("oopsie");
+  }
+}
