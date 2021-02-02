4093a8161bbabe7f0bd7abf2a1914f0ffb2f6e54
booshja333 <jacob.andes@gmail.com>
2021-02-02 13:00:54 -0800

    finished flask-madlibs exercise

--- Python/flask-madlibs/app.py
+++ Python/flask-madlibs/app.py
@@ -1,1 +1,19 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
+from stories import story
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    """Displays landing page for the site"""
+
+    return render_template('questions.html', words_list=story.prompts)
+
+
+@app.route('/story')
+def story_display():
+    """Displays the rendered story from the user's input"""
+    answers = request.args
+    madlib = story.generate(answers)
+    return render_template('story_display.html', madlib=madlib)
