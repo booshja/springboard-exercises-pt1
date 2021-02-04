b982c841cc781c56c3ed2fcbd0eecaab853c8386
booshja333 <jacob.andes@gmail.com>
2021-02-03 18:24:04 -0800

    finished flask-survey exercise minus session

---
+++ Python/flask-survey/app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, redirect, flash, request
+from flask_debugtoolbar import DebugToolbarExtension
+from surveys import surveys
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "this-is-secret"
+debug = DebugToolbarExtension(app)
+
+responses = []
+current_page = 0
+
+
+@app.route('/')
+def landing_page():
+    return render_template('home.html')
+
+
+@app.route('/questions/<quest_num>')
+def questions_page(quest_num):
+    global current_page
+    quest_num = int(quest_num)
+    if quest_num != current_page:
+        flash("Invalid Question. Please answer the questions in order.", "error")
+        return redirect('/questions/' + str(current_page))
+    elif quest_num == len(surveys["satisfaction"].questions):
+        return redirect('/thank-you')
+    else:
+        curr_quest_num = current_page
+        title = surveys["satisfaction"].title
+        instructions = surveys["satisfaction"].instructions
+        question = surveys["satisfaction"].questions[quest_num]
+        return render_template('questions.html', quest_num=curr_quest_num, title=title, instructions=instructions, question=question)
+
+
+@app.route('/answer')
+def answer():
+    global current_page
+    current_page += 1
+    answer = request.args["question-form"]
+    responses.append(answer)
+    return redirect('/questions/' + str(current_page))
+
+
+@app.route('/thank-you')
+def thank_you():
+    return render_template('thank_you.html')
