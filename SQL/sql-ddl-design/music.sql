24dc7afcc9b66cf589e44975118868efa82a7e53
Jacob <jacob.andes@gmail.com>
2021-02-18 10:32:39 -0800

    finished last of sql-ddl-exercise

--- SQL/sql-ddl-design/music.sql
+++ SQL/sql-ddl-design/music.sql
@@ -4,6 +4,12 @@ CREATE DATABASE music;
 
 \c music
 
+CREATE TABLE albums
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
 CREATE TABLE songs
 (
   id SERIAL PRIMARY KEY,
@@ -11,20 +17,23 @@ CREATE TABLE songs
   duration_in_seconds INTEGER NOT NULL,
   release_date DATE NOT NULL,
   artists TEXT[] NOT NULL,
-  album TEXT NOT NULL,
+  album INTEGER NOT NULL REFERENCES albums,
   producers TEXT[] NOT NULL
 );
+
+INSERT INTO albums VALUES
+('Middle of Nowhere'), ('A Night at the Opera'), ('Daydream'), ('A Star Is Born'), ('Silver Side Up'), ('The Blueprint 3'), ('Prism'), ('Hands All Over'), ('Let Go'), ('The Writing''s on the Wall');
 
 INSERT INTO songs
   (title, duration_in_seconds, release_date, artists, album, producers)
 VALUES
-  ('MMMBop', 238, '04-15-1997', '{"Hanson"}', 'Middle of Nowhere', '{"Dust Brothers", "Stephen Lironi"}'),
-  ('Bohemian Rhapsody', 355, '10-31-1975', '{"Queen"}', 'A Night at the Opera', '{"Roy Thomas Baker"}'),
-  ('One Sweet Day', 282, '11-14-1995', '{"Mariah Cary", "Boyz II Men"}', 'Daydream', '{"Walter Afanasieff"}'),
-  ('Shallow', 216, '09-27-2018', '{"Lady Gaga", "Bradley Cooper"}', 'A Star Is Born', '{"Benjamin Rice"}'),
-  ('How You Remind Me', 223, '08-21-2001', '{"Nickelback"}', 'Silver Side Up', '{"Rick Parashar"}'),
-  ('New York State of Mind', 276, '10-20-2009', '{"Jay Z", "Alicia Keys"}', 'The Blueprint 3', '{"Al Shux"}'),
-  ('Dark Horse', 215, '12-17-2013', '{"Katy Perry", "Juicy J"}', 'Prism', '{"Max Martin", "Cirkut"}'),
-  ('Moves Like Jagger', 201, '06-21-2011', '{"Maroon 5", "Christina Aguilera"}', 'Hands All Over', '{"Shellback", "Benny Blanco"}'),
-  ('Complicated', 244, '05-14-2002', '{"Avril Lavigne"}', 'Let Go', '{"The Matrix"}'),
-  ('Say My Name', 240, '11-07-1999', '{"Destiny''s Child"}', 'The Writing''s on the Wall', '{"Darkchild"}');
+  ('MMMBop', 238, '04-15-1997', '{"Hanson"}', 1, '{"Dust Brothers", "Stephen Lironi"}'),
+  ('Bohemian Rhapsody', 355, '10-31-1975', '{"Queen"}', 2, '{"Roy Thomas Baker"}'),
+  ('One Sweet Day', 282, '11-14-1995', '{"Mariah Cary", "Boyz II Men"}', 3, '{"Walter Afanasieff"}'),
+  ('Shallow', 216, '09-27-2018', '{"Lady Gaga", "Bradley Cooper"}', 4, '{"Benjamin Rice"}'),
+  ('How You Remind Me', 223, '08-21-2001', '{"Nickelback"}', 5, '{"Rick Parashar"}'),
+  ('New York State of Mind', 276, '10-20-2009', '{"Jay Z", "Alicia Keys"}', 6, '{"Al Shux"}'),
+  ('Dark Horse', 215, '12-17-2013', '{"Katy Perry", "Juicy J"}', 7, '{"Max Martin", "Cirkut"}'),
+  ('Moves Like Jagger', 201, '06-21-2011', '{"Maroon 5", "Christina Aguilera"}', 8, '{"Shellback", "Benny Blanco"}'),
+  ('Complicated', 244, '05-14-2002', '{"Avril Lavigne"}', 9, '{"The Matrix"}'),
+  ('Say My Name', 240, '11-07-1999', '{"Destiny''s Child"}', 10, '{"Darkchild"}');
