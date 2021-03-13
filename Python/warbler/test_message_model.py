486ae661488104dcd0024a544a9f824e51e36fda
Jacob <jacob.andes@gmail.com>
2021-03-12 22:30:52 -0800

    cleaned up test_messages_model

--- Python/warbler/test_message_model.py
+++ Python/warbler/test_message_model.py
@@ -78,13 +78,3 @@ class MessageModelTestCase(TestCase):
             error = True
 
         self.assertEqual(error, True)
-
-# Does is_following successfully detect when user1 is following user2?
-# Does is_following successfully detect when user1 is not following user2?
-# Does is_followed_by successfully detect when user1 is followed by user2?
-# Does is_followed_by successfully detect when user1 is not followed by user2?
-# Does User.create successfully create a new user given valid credentials?
-# Does User.create fail to create a new user if any of the validations(e.g. uniqueness, non-nullable fields) fail?
-# Does User.authenticate successfully return a user when given a valid username and password?
-# Does User.authenticate fail to return a user when the username is invalid?
-# Does User.authenticate fail to return a user when the password is invalid?
